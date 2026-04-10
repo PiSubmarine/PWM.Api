16a91e12766bf75133390dcbdd2e3c2ef7893730
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-04-10 21:50:39 +0200

    Get/Set frequency methods added

--- mock/PiSubmarine/PWM/Api/IDriverMock.h
+++ mock/PiSubmarine/PWM/Api/IDriverMock.h
@@ -9,8 +9,10 @@ namespace PiSubmarine::PWM::Api
     class IDriverMock : public IDriver
     {
 public:
-        MOCK_METHOD(NormalizedFraction, GetDutyCycle, (), (const, override));
-        MOCK_METHOD(void, SetDutyCycle, (NormalizedFraction duty), (override));
+        MOCK_METHOD((std::expected<Hertz, Error>), GetFrequency, (), (const, override));
+        MOCK_METHOD(Error, SetFrequency, (Hertz frequency), (override));
+        MOCK_METHOD((std::expected<NormalizedFraction, Error>), GetDutyCycle, (), (const, override));
+        MOCK_METHOD(Error, SetDutyCycle, (NormalizedFraction duty), (override));
     };
 }
 
--- public/PiSubmarine/PWM/Api/IDriver.h
+++ public/PiSubmarine/PWM/Api/IDriver.h
@@ -1,15 +1,29 @@
 #pragma once
 
+#include <expected>
 #include "PiSubmarine/NormalizedFraction.h"
+#include "PiSubmarine/Hertz.h"
 
 namespace PiSubmarine::PWM::Api
 {
     class IDriver
     {
 public:
+        enum class Error
+        {
+            Ok,
+            ProtocolError,
+            UnsupportedParameter,
+            UnknownError
+        };
+
         virtual ~IDriver() = default;
-        [[nodiscard]] virtual NormalizedFraction GetDutyCycle() const = 0;
-        virtual void SetDutyCycle(NormalizedFraction duty) = 0;
+
+        [[nodiscard]] virtual std::expected<Hertz, Error> GetFrequency() const = 0;
+        [[nodiscard]] virtual Error SetFrequency(Hertz frequency) = 0;
+
+        [[nodiscard]] virtual std::expected<NormalizedFraction, Error> GetDutyCycle() const = 0;
+        virtual Error SetDutyCycle(NormalizedFraction duty) = 0;
     };
 }
 
